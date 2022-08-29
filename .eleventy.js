adc1726dea18c27ea27060aca27b25c71e201326
Fershad <itsfish@fershad.com>
2022-08-29 21:19:12 +0800

    add configuration

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
 const dev = process.env.NODE_ENV === 'production' ? false : true;
 
 module.exports = function(eleventyConfig) {
+
+    if (!dev) {
+        eleventyConfig.ignores.add("src/design/**");
+    }
     // Return your Object options:
     return {
       dir: {
