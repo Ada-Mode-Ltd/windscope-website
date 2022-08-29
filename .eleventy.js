823f81daae443cf527820400a76aae79b90a6006
Fershad <itsfish@fershad.com>
2022-08-29 23:11:10 +0800

    create the start of our design system

--- .eleventy.js
+++ .eleventy.js
@@ -5,11 +5,19 @@ module.exports = function(eleventyConfig) {
     if (!dev) {
         eleventyConfig.ignores.add("src/design/**");
     }
+
+    eleventyConfig.addPassthroughCopy({
+      public: './'
+    })
+
     // Return your Object options:
     return {
       dir: {
         input: "src",
-        output: dev ? "_dev" : "_site"
+        output: dev ? "_dev" : "_site",
+        includes: "_includes",
+        layouts: "_layouts",
+        htmlTemplateEngine: "liquid",
       }
     }
   };
