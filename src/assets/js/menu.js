3027f905090c764e4ce7e0ebe92a6681073fb6ac
Fershad <itsfish@fershad.com>
2022-09-01 18:06:34 +0800

    fix menu

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,13 +1,11 @@
 import * as focusTrap from 'focus-trap'
 
 const header = document.querySelector('[data-page-nav]')
-const menuButtonText = header.querySelector('[data-menu-button-text]')
 const menuIconOpen = header.querySelector('[data-menu-icon-open]')
 const menuIconClose = header.querySelector('[data-menu-icon-close]')
 const menuBtn = header.querySelector('[data-btn="menu"]')
 const menuContainer = header.querySelector('[data-menu-container]')
 const menuWrapper = header.querySelector('[data-menu-wrapper]')
-const homeLink = header.querySelector('[data-home-link]')
 
 const open = () => {
   menuWrapper.hidden = false
@@ -36,7 +34,6 @@ const close = () => {
     menuIconClose.hidden = true
     menuIconOpen.hidden = false
     menuBtn.setAttribute('aria-expanded', false)
-    // menuButtonText.innerText = 'Menu'
   }, 250)
 }
 
@@ -56,7 +53,6 @@ const menu = () => {
   menuWrapper.hidden = true
   menuIconClose.hidden = true
   menuIconOpen.hidden = false
-  homeLink.hidden = false
   menuIconClose.style.display = 'none'
   menuWrapper.classList.add('js-menu')
   menuBtn.addEventListener('click', toggleMenu)
