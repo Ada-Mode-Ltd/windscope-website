4f8197c98955ca4cb7df5ce47c87c78012e576b5
Fershad <itsfish@fershad.com>
2022-09-27 12:15:50 +0800

    single quote on homepage

--- src/utils/getHomepage.js
+++ src/utils/getHomepage.js
@@ -2,7 +2,10 @@ const { client } = require('./sanity');
 
 
 async function getHomepage() {
-    const query = '*[_type == "wsHomepage" && !(_id in path("drafts.**"))][0]{ ... }'
+    const query = `*[_type == "wsHomepage" && !(_id in path("drafts.**"))][0]{ 
+        ...,
+        "quotes": quotes[]->{..., partner->{...}},
+     }`
     const docs = await client.fetch(query).catch(err => console.error(err));
     return docs;
 }
