33a8b3940042fa3a593152fea0ebade1f6d6acf9
Fershad <itsfish@fershad.com>
2022-09-29 16:04:00 +0800

    setup homepage feature

--- src/utils/getHomepage.js
+++ src/utils/getHomepage.js
@@ -4,6 +4,7 @@ const { client } = require('./sanity');
 async function getHomepage() {
     const query = `*[_type == "wsHomepage" && !(_id in path("drafts.**"))][0]{ 
         ...,
+        "feature": feature->{...},
         "quotes": quotes[]->{..., partner->{...}},
      }`
     const docs = await client.fetch(query).catch(err => console.error(err));
