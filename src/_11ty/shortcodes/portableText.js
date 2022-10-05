97b57452562fe8cf34a57d7743ba9c2cfdaa17a8
Fershad <itsfish@fershad.com>
2022-10-05 12:28:14 +0800

    add text highlight marks

--- src/_11ty/shortcodes/portableText.js
+++ src/_11ty/shortcodes/portableText.js
@@ -1,11 +1,28 @@
 const { toHTML } = require('@portabletext/to-html')
 
+const htm = require('htm')
+const vhtml = require('vhtml')
+const html = htm.bind(vhtml)
+
+const serializer = {
+    // types: {
+    //   image: ({value}) => html`<img src="${value.imageUrl}" />`,
+    //   callToAction: ({value, isInline}) =>
+    //     isInline
+    //       ? html`<a href="${value.url}">${value.text}</a>`
+    //       : html`<div class="callToAction">${value.text}</div>`,
+    // },
+  
+    marks: {
+        highlightBlue: ({children}) => html`<span class="color-primary">${children}</span>`,
+        highlightGreen: ({children}) => html`<span class="color-accent">${children}</span>`,
+    },
+}
+
 const portableText = (text) => {
-    const html = toHTML(text, {
-        serializers: {
-        },
-    })
-    return html
+    const output = toHTML(text, {components: serializer})
+    
+    return output
 }
 
 module.exports = portableText
