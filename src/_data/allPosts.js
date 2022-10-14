d98344891cfe27060e3cf0181e3a86172e265ad2
Fershad <itsfish@fershad.com>
2022-10-14 17:20:42 +0800

    style the category navigation

--- src/_data/allPosts.js
+++ src/_data/allPosts.js
@@ -5,5 +5,16 @@ module.exports = async function() {
     const features = await data.filter(doc => doc.featured);
     const feature = features ? features[0] : null;
     const posts = data.filter(doc => doc._id !== feature?._id);
-    return { feature, posts };
+
+    // Get an array of the uniques categories used in each post
+    const categories = posts.reduce((acc, post) => {
+        post.categories.forEach(cat => {
+            if (!acc.includes(cat.title)) {
+                acc.push(cat.title);
+            }
+        });
+        return acc;
+    }, []);
+
+    return { feature, posts, categories };
 };
