ef39ced75badc35a45946ad6ba27e0049c5cda37
Fershad <itsfish@fershad.com>
2022-10-17 18:01:14 +0800

    simple check for gif images

--- src/_11ty/shortcodes/image.js
+++ src/_11ty/shortcodes/image.js
@@ -1,10 +1,14 @@
 const Image = require("@11ty/eleventy-img");
 
 async function imageShortcode(src, alt, sizes) {
+  const formats = src.includes('.gif') ? ['webp', 'gif'] : ["avif", "webp", "svg", "jpeg"]
   let metadata = await Image(src, {
-    formats: ["avif", "webp", "svg", "jpeg"],
+    formats,
     urlPath: "/assets/img/remote/",
     outputDir: "./public/assets/img/remote/",
+    sharpOptions: {
+      animated: true
+    },
     svgShortCircuit: true,
         cacheOptions: {
           duration: "30d",
