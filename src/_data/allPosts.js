0d1b34e504fe860357c98e2d0ab846268dd4164f
Fershad <itsfish@fershad.com>
2022-10-18 17:38:10 +0800

    use latest post if feature not set

--- src/_data/allPosts.js
+++ src/_data/allPosts.js
@@ -3,8 +3,8 @@ const allPosts = require('../utils/getPosts');
 module.exports = async function() {
     const data = await allPosts();
     const features = await data.filter(doc => doc.featured);
-    const feature = features ? features[0] : null;
-    const posts = data.filter(doc => doc._id !== feature?._id);
+    const feature = features.length > 0 ? features[0] : data[0];
+    const posts = data.filter(doc => doc._id !== feature?._id).slice(0, 4);
 
     // combine feature and posts
     const combined = [feature, ...posts];
