49697b4c7c6751b1c1d45dba7e979360f47d5396
Fershad Irani <27988517+fershad@users.noreply.github.com>
2022-11-15 07:13:14 +0800

    add jobs pages

--- src/utils/getJobs.js
+++ src/utils/getJobs.js
@@ -4,6 +4,19 @@ const { client } = require('./sanity');
 async function getPeople() {
     const query = `*[_type == "job" && "ws" in publishTo && open && !(_id in path("drafts.**"))]{ 
         ...,
+        body{
+            ...,
+            content[]{
+                ...,
+                markDefs[] {
+                  ...,
+                  _type == "internalLink" => {
+                    ...,
+                    "slug": @.reference-> slug
+                  }
+                }
+            }
+          } 
      }`
     const docs = await client.fetch(query).catch(err => console.error(err));
     return docs;
