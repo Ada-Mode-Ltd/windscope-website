7c96eae99393405293cfae4100b7bf2633738d5d
Fershad Irani <27988517+fershad@users.noreply.github.com>
2022-11-21 18:30:38 +0800

    no sanity cdn

--- src/utils/sanity.js
+++ src/utils/sanity.js
@@ -4,7 +4,7 @@ const client = sanityClient({
   dataset: 'production',
   apiVersion: '2022-09-26', // use current UTC date - see "specifying API version"!
 //   token: 'sanity-auth-token', // or leave blank for unauthenticated usage
-  useCdn: true, // `false` if you want to ensure fresh data
+  useCdn: false, // `false` if you want to ensure fresh data
 })
 
 module.exports = { client }
