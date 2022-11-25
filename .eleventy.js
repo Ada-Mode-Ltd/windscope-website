6746463944f6d6b51e3c03e76bc39c3425e570d7
Fershad Irani <27988517+fershad@users.noreply.github.com>
2022-11-25 18:45:16 +0800

    placeholder folder

--- .eleventy.js
+++ .eleventy.js
@@ -157,7 +157,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/_includes/assets/css");
   // eleventyConfig.addPassthroughCopy({ "public/assets/build.css": "assets/preview/index.css" });
   eleventyConfig.addPassthroughCopy("src/_includes/assets/js");
-  eleventyConfig.addPassthroughCopy("public/assets/**");
+  eleventyConfig.addPassthroughCopy("public/assets/**/*");
 
 
   eleventyConfig.on("eleventy.after", async () => {
